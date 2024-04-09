b2b6e8c54b64a21970a41a46fadcad6d9217c671
iyungui <yungui632@gmail.com>
2024-04-09 23:59:20 +0900

    240409 commit

--- Swift/흐름제어.swift
+++ Swift/흐름제어.swift
@@ -30,3 +30,35 @@ default:
     print("Some other character")
 }
 
+// 와일드카드 식별자를 사용한 튜플 switch case 구성
+typealias NameAge = (name: String, age: Int)
+
+let tupleValue: NameAge = ("yungui", 99)
+
+switch tupleValue {
+case ("yungui", 50):
+    print("정확히 맞췄습니다")
+case ("yungui", _):
+    print("이름만 맞았습니다. 나이는 \(tupleValue.age)입니다.")
+case (_, 99):
+    print("나이만 맞았습니다. 이름은 \(tupleValue.name)입니다.")
+default:
+    print("누굴 찾나요?")
+}
+
+
+
+// 값 바인딩을 사용한 튜플 switch case 구성
+switch tupleValue {
+case ("yungui", 50):
+    print("정확히 맞췄습니다")
+case ("yungui", let age):
+    print("이름만 맞았습니다. 나이는 \(age)입니다.")
+case (let name, 99):
+    print("나이만 맞았습니다. 이름은 \(name)입니다.")
+default:
+    print("누굴 찾나요?")
+}
+
+
+// 이름만 맞았습니다. 나이는 99입니다.
