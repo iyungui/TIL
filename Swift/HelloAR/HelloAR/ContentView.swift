51fbfbacd36e11faf628a1951962ebd80715a615
iyungui <rsdbddml@gmail.com>
2024-05-17 21:50:51 +0900

    creating my first AR project

---
+++ Swift/HelloAR/HelloAR/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  HelloAR
+//
+//  Created by 이융의 on 5/17/24.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#Preview {
+    ContentView()
+}
