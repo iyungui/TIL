983b2f8f2664b8957a5778c1eaa49ca90ca4a779
iyungui <rsdbddml@gmail.com>
2024-05-19 00:14:59 +0900

    Adding Multiple Model Entities

--- Swift/HelloAR/HelloAR/ContentView.swift
+++ Swift/HelloAR/HelloAR/ContentView.swift
@@ -25,7 +25,15 @@ struct ARViewContainer: UIViewRepresentable {
         let material = SimpleMaterial(color: .blue, isMetallic: true)
         let box = ModelEntity(mesh: MeshResource.generateBox(size: 0.3), materials: [material])
         
+        let sphere = ModelEntity(mesh: MeshResource.generateSphere(radius: 0.3), materials: [SimpleMaterial(color: .yellow, isMetallic: true)])
+        sphere.position = simd_float3(0, 0.4, 0)
+        
+        let plane = ModelEntity(mesh: MeshResource.generatePlane(width: 0.5, depth: 0.3), materials: [SimpleMaterial(color: .red, isMetallic: true)])
+        plane.position = simd_float3(0, 0.7, 0)
+        
         anchor.addChild(box)
+        anchor.addChild(sphere)
+        anchor.addChild(plane)
         arView.scene.anchors.append(anchor)
         
         return arView
